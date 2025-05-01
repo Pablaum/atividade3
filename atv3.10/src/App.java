eaf1868bd6d13707e364ba11b2515c9c788c699e
PabloFR <pablor3252@gmail.com>
2025-05-01 14:38:01 -0300

    Update App.java

--- atv3.10/src/App.java
+++ atv3.10/src/App.java
@@ -1,32 +1,26 @@
 import java.util.Scanner;
-
+// observação: fiz essa atividade pelo replit, pois o terminal do vscode estava rodando o codigo da atv anterior (???)
 public class App {
     public static void main(String[] args) throws Exception {
         Scanner scanner = new Scanner(System.in);
 
         int[] numeros = new int[10];
 
-        // Leitura dos 10 números
-        System.out.println("Digite 10 números inteiros:");
+        System.out.println("insira 10 números inteiros:");
         for (int i = 0; i < numeros.length; i++) {
             System.out.print("Número " + (i + 1) + ": ");
             numeros[i] = scanner.nextInt();
         }
-
-        // Ordenando com Bubble Sort
         for (int i = 0; i < numeros.length - 1; i++) {
             for (int j = 0; j < numeros.length - 1 - i; j++) {
                 if (numeros[j] > numeros[j + 1]) {
-                    // Troca os elementos
                     int temp = numeros[j];
                     numeros[j] = numeros[j + 1];
                     numeros[j + 1] = temp;
                 }
             }
         }
-
-        // Exibindo o vetor ordenado
-        System.out.println("\nVetor ordenado em ordem crescente:");
+        System.out.println("vetor ordenado em ordem crescente:");
         for (int num : numeros) {
             System.out.print(num + " ");
         }
